089aa0222c90f4bd22abafa1218c99a8495979c1
Utsav Kumar <utsavkumar.ca@gmail.com>
2024-11-28 16:32:04 -0800

    #5 - Setup and Install Jest for Unit Testing
    Installed and configured jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
